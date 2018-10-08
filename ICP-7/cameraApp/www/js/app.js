4bcf8c9918e9749ee74d269ce764b9ecf4c5990e
benamreview <dhh3hb@mail.umkc.edu>
2018-10-07 19:15:11 -0500

    Migrated to new cameraApp

---
+++ ICP-7/cameraApp/www/js/app.js
@@ -0,0 +1,113 @@
+var imageApp=angular.module("starter", ["ionic","ngCordova","firebase"]);
+
+imageApp.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+imageApp.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state("firebase", {
+            url: "/firebase",
+            templateUrl: "templates/firebase.html",
+            controller: "FirebaseController",
+            cache: false
+        })
+        .state("secure", {
+            url: "/secure",
+            templateUrl: "templates/secure.html",
+            controller: "SecureController"
+        });
+    $urlRouterProvider.otherwise('/firebase');
+});
+
+imageApp.controller("FirebaseController", function($scope, $state, $firebaseAuth) {
+
+    var fbAuth = $firebaseAuth();
+    var provider = new firebase.auth.GoogleAuthProvider();
+    $scope.login = function(username, password) {
+        fbAuth.$signInWithEmailAndPassword(username,password).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+    $scope.Glogin = function (){
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        $state.go("secure");
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    }
+    $scope.register = function(username, password) {
+        fbAuth.$createUserWithEmailAndPassword(username,password).then(function(userData) {
+            return fbAuth.$signInWithEmailAndPassword(username,
+                password);
+        }).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+
+});
+
+//secure controller
+
+imageApp.controller("SecureController", function($scope, $ionicHistory, $firebaseObject, $firebaseArray, $firebaseAuth, $cordovaCamera,$state) {
+
+    $ionicHistory.clearHistory();  //for clearing user login history
+
+    $scope.images = [];
+    $scope.fb = $firebaseAuth();
+    var fbAuth = $scope.fb.$getAuth();
+    var ref = firebase.database().ref();
+    var obj = $firebaseObject(ref);
+    if(fbAuth) {
+        var userReference = ref.child("users/" + fbAuth.uid);   //capture the user reference in data structure ,it navigates to specific user page in freebase
+        var syncArray = $firebaseArray(userReference.child("images"));  //binding specific node in firebase to an array object in angularjs
+        $scope.images = syncArray;
+    } else {
+        $state.go("firebase");  //directs to firebase page
+    }
+
+    $scope.upload = function() {
+        var options = {
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+            targetWidth: 500,
+            targetHeight: 500,
+            saveToPhotoAlbum: false
+        };
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            syncArray.$add({image: imageData}).then(function() {
+                alert("Image has been uploaded");
+            });
+        }, function(error) {
+            console.error(error);
+        });
+    }
+
+});
